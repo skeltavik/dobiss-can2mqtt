d1c5888988f07719d3cff4c32398b3742c89c66d
Bram Ruttens <mail@bramruttens.be>
2023-07-16 23:06:34 +0200

    Add initial try polling mechanism

--- can2mqtt.py
+++ can2mqtt.py
@@ -2,6 +2,7 @@ import can
 import time
 import yaml
 import paho.mqtt.client as mqtt
+import threading
 
 # Load the configuration file
 with open('config.yaml') as f:
@@ -15,10 +16,15 @@ bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=125000)
 client = mqtt.Client()
 client.connect("localhost", 1883)  # Replace with your MQTT broker URL
 
-# Function to send a CAN message
-def send_can_message(module, relay, state):
+# Function to send a CAN message and get a response
+def send_can_message_and_get_response(module, relay, state=None):
     # Construct the CAN data
-    data = [module, relay, state, 0xFF, 0xFF] if state else [module, relay, 0, 0xFF, 0xFF]
+    if state is not None:
+        # If a state is provided, send a CAN message to set the state of the light
+        data = [module, relay, state, 0xFF, 0xFF]
+    else:
+        # If no state is provided, send a CAN message to get the state of the light
+        data = [module, relay, 0xFF, 0xFF, 0xFF]
 
     # Create a CAN message
     message = can.Message(arbitration_id=0x01FC0002 | (module << 8), data=data, is_extended_id=True)
@@ -26,6 +32,22 @@ def send_can_message(module, relay, state):
     # Send the CAN message
     print(f"Sending CAN message: {message}")
     bus.send(message)
+
+    # If no state is provided, wait for a response from the Dobiss system and parse the response to get the state of the light
+    if state is None:
+        # Wait for a response from the Dobiss system
+        response = bus.recv(1.0)  # wait up to 1 second
+
+        # Check if the response has the correct arbitration ID
+        if response is not None and response.arbitration_id == 0x01FDFF01:
+            # Extract the state from the data
+            state = response.data[0]
+
+            # Publish the state to the MQTT broker
+            address = f"{module:02X}{relay:02X}"
+            client.publish(f"dobiss/light/{address}/state", str(state))
+
+    return state
 
 # Function to control a light
 def control_light(address, state):
@@ -40,11 +62,24 @@ def control_light(address, state):
 
             # Send the CAN message
             print(f"Controlling light: {light['name']} (address: {address}, state: {state})")
-            send_can_message(module, relay, state)
+            send_can_message_and_get_response(module, relay, state)
             return light['name']
     else:
         print(f"No light found with address: {address}")
         return None
+
+# Function to poll the state of each light
+def poll_light_states():
+    for light in config['lights']:
+        # Convert the address to a module and relay
+        module = int(light['address'][:2], 16)
+        relay = int(light['address'][2:], 16)
+
+        # Get the state of the light
+        send_can_message_and_get_response(module, relay)
+
+    # Schedule the next poll
+    threading.Timer(0.5, poll_light_states).start()
 
 # The on_connect callback function
 def on_connect(client, userdata, flags, rc):
@@ -86,6 +121,9 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.loop_start()
+
+# Start polling
+poll_light_states()
 
 while True:
     time.sleep(1)  # Keep the script running
